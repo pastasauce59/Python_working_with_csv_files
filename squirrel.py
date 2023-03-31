75f7e7c7c8e7641d4eaba432ee6613fa52cb10f7
Michael Primavera <pastasauce59@gmail.com>
2023-03-30 23:48:15 -0400

    Added Central Park Squirrel data CSV file, created squirrel.py to extract data from CSV by getting the total counts of each squirrel by their fur color.

---
+++ squirrel.py
@@ -0,0 +1,20 @@
+import pandas
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# print(data['Primary Fur Color'])
+
+#Getting unique values of squirrel colors from csv (for reference)
+squirrel_colors = []
+for color in data['Primary Fur Color']:
+    if color not in squirrel_colors:
+        squirrel_colors.append(color)
+squirrel_colors.pop(0)
+# print(squirrel_colors)
+
+squirrel_fur_color_dict = {}
+for color in squirrel_colors:
+    squirrel_fur_color_dict[color] = (data['Primary Fur Color'] == color).sum()
+print(squirrel_fur_color_dict)
+
+# squirrel_count_data = pandas.DataFrame(squirrel_fur_color_dict)
+# squirrel_count_data.to_csv('squirrel_count.csv')
