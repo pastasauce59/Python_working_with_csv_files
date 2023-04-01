1eed5f92ed5c802846cacaddec40f38b6807cdf1
Michael Primavera <pastasauce59@gmail.com>
2023-03-31 21:28:20 -0400

    Updated notes for extracting certain kinds of data from ccsv's.

--- squirrel.py
+++ squirrel.py
@@ -1,15 +1,20 @@
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-# print(data['Primary Fur Color'])
+# --> Gets every gray squirrel row
+# print(data[data['Primary Fur Color'] == 'Gray'])
+
+# --> Returns True or False for a gray squirrel from every row
+# print(data['Primary Fur Color'] == 'Gray')
 
 #Getting unique values of squirrel colors from csv (for reference)
 squirrel_colors = []
 for color in data['Primary Fur Color']:
     if color not in squirrel_colors:
         squirrel_colors.append(color)
+#To remove 'Nan' from squirrel_colors[0] and contain only actual colors
 squirrel_colors.pop(0)
-# print(squirrel_colors)
+
 
 squirrel_fur_color_dict = {}
 squirrel_fur_color_dict['Fur Color'] = []
